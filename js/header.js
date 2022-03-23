ab53ef99200878fbe21a708f4b7a9ceb2da2c55c
innabatalova <batalovainna97@gmail.com>
2022-03-23 19:34:11 +0300

    Create: added style for main

--- js/header.js
+++ js/header.js
@@ -1,36 +1,40 @@
-const navigationIconMap = $(".navigation__icon__map");
-const navigationAddress = $(".navigation__address");
-const navigationIconCall = $(".navigation__icon__call");
-const navigationCall = $(".navigation__call");
+const headerContacts = () => {
+  const navigationIconMap = $(".navigation__icon__map");
+  const navigationAddress = $(".navigation__address");
+  const navigationIconCall = $(".navigation__icon__call");
+  const navigationCall = $(".navigation__call");
 
-navigationIconMap.on("mouseover", function () {
-  navigationAddress.addClass("flex");
-  navigationAddress.show();
-});
+  navigationIconMap.on("mouseover", function () {
+    navigationAddress.addClass("flex");
+    navigationAddress.show();
+  });
 
-$(document).mouseout(function (e) {
-  if (
-    !navigationIconMap.is(e.target) &&
-    !navigationAddress.is(e.target) &&
-    navigationAddress.has(e.target).length === 0
-  ) {
-    navigationAddress.removeClass("flex");
-    navigationAddress.hide();
-  }
-});
+  $(document).mouseout(function (e) {
+    if (
+      !navigationIconMap.is(e.target) &&
+      !navigationAddress.is(e.target) &&
+      navigationAddress.has(e.target).length === 0
+    ) {
+      navigationAddress.removeClass("flex");
+      navigationAddress.hide();
+    }
+  });
 
-navigationIconCall.on("mouseover", function () {
-  navigationCall.addClass("flex");
-  navigationCall.show();
-});
+  navigationIconCall.on("mouseover", function () {
+    navigationCall.addClass("flex");
+    navigationCall.show();
+  });
 
-$(document).mouseout(function (e) {
-  if (
-    !navigationIconCall.is(e.target) &&
-    !navigationCall.is(e.target) &&
-    navigationCall.has(e.target).length === 0
-  ) {
-    navigationCall.removeClass("flex");
-    navigationCall.hide();
-  }
-});
+  $(document).mouseout(function (e) {
+    if (
+      !navigationIconCall.is(e.target) &&
+      !navigationCall.is(e.target) &&
+      navigationCall.has(e.target).length === 0
+    ) {
+      navigationCall.removeClass("flex");
+      navigationCall.hide();
+    }
+  });
+};
+
+headerContacts();
