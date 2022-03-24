1caa9ad267d2d9c0ed718d3cee3b366766925bb0
innabatalova <batalovainna97@gmail.com>
2022-03-24 22:44:54 +0300

    Fix: check script for examples

--- js/example.js
+++ js/example.js
@@ -2,8 +2,11 @@ const examples = () => {
   const examplesCard = $(".examples__card");
   const examplesCardInfo = $(".examples__card__info");
 
-  examplesCard.on("mouseover", function () {
-    examplesCardInfo.css("opacity", "1");
+  $(document).on("mouseover", function (e) {
+    if (examplesCard.is(e.target)) {
+      let p = $(e.target.children[1]);
+      p.css("opacity", "1");
+    }
   });
 
   examplesCard.on("mouseout", function () {
